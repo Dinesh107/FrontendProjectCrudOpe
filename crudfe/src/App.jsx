7f2cdb1beb8e70efd2bfb0af822747197200347e
Dinesh107 <dineshjr107@gmail.com>
2026-01-19 20:28:33 +0530

    second commit

--- crudfe/src/App.jsx
+++ crudfe/src/App.jsx
@@ -5,7 +5,9 @@ import axios from "axios";
 function App() {
   const [users, setUsers] = useState([]);
   const [filteredUsers, setfilteredUsers] = useState([]);
-
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [userData, setUserData] = useState({ name: "", age: "", city: "" });
+  // passthis data to the api
   const getAllUsers = async () => {
     await axios.get("http://localhost:8000/users").then((res) => {
       setUsers(res.data); // before filterd it show all the data
@@ -22,12 +24,48 @@ function App() {
 
   const handleSearchChange = (e) => {
     const searchText = e.target.value.toLowerCase();
-    const filteredUsers = users.filter((user) =>
+    const filteredUsers = users.filter(
+      (user) =>
         user.name.toLowerCase().includes(searchText) ||
-        user.city.toLowerCase().includes(searchText)) 
- 
+        user.city.toLowerCase().includes(searchText),
+    );
+
     setfilteredUsers(filteredUsers);
   };
+
+  // Delete
+
+  const handleDelete = async (id) => {
+    const isConfirmed = window.confirm(
+      "Ar you sure you wnay to delete this user?",
+    );
+    if (isConfirmed) {
+      await axios.delete(`http://localhost:8000/users/${id}`).then((res) => {
+        setUsers(res.data); // before filterd it show all the data
+        setfilteredUsers(res.data); // after fitered  it show the filterd data
+      });
+    }
+  };
+
+  // add user deails
+
+  const handleAddRecord = () => {
+    setUserData({ name: "", age: "", city: "" }); // resetting
+    setIsModalOpen(true);
+  };
+  
+
+  // close modal
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  }
+
+  // handledata
+
+  const handleData = (e) => {
+     setUserData({...userData, [e.target.name]: e.target.value})
+  }
 
   return (
     <>
@@ -39,7 +77,9 @@ function App() {
             placeholder="Search"
             onChange={handleSearchChange}
           />
-          <button className="btn green">Add</button>
+          <button onClick={handleAddRecord} className="btn green">
+            Add
+          </button>
           <table className="table">
             <thead>
               <tr>
@@ -64,13 +104,39 @@ function App() {
                         <button className="btn  green">Edit</button>
                       </td>
                       <td>
-                        <button className="btn red">Delete</button>
+                        <button
+                          onClick={() => handleDelete(user.id)}
+                          className="btn red"
+                        >
+                          Delete
+                        </button>
                       </td>
                     </tr>
                   );
                 })}
             </tbody>
           </table>
+          {isModalOpen && (
+            <div className="modal">
+              <div className="modal-content">
+                <span className="close" onClick={closeModal} >&times;</span>
+                <h2>User Record</h2>
+                <div className="input-group">
+                  <label htmlFor="name">Ful name </label>
+                  <input type="text" value={userData.name} onChange={handleData} name="name" id="name" />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="age">Age </label>
+                  <input type="number" value={userData.age} onChange={handleData} name="age" id="age" />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="city">City </label>
+                  <input type="text" value={userData.city} onChange={handleData} name="city" id="city" />
+                </div>
+                <button className="btn green" >Add User</button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const users = require("./sample.json");
 const cors = require("cors");
+const fs = require("fs");
 const app = express();
+
 
 const port = 8000;
 
@@ -11,6 +13,17 @@ app.use(cors()); // middleware - to cors issue
 // Display All user 
 app.get("/users", (req, res) => {
     return res.json(users);
+})
+
+// Delete users dataila
+
+app.delete("/users/:id", (req, res) => {
+    let id = Number(req.params.id);
+    let filteredUsers = users.filter((user) => user.id!==id);
+    fs.writeFile("./sample.json", JSON.stringify(filteredUsers), () => {
+    let filteredUsers = users.filter((user) => user.id!==id);
+        return res.json(filteredUsers);
+    })
 })
 
 //http://localhost:8000/users
